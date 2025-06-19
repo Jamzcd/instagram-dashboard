845b505837a50b8b652deb4201a4633d3a7fd172
JAMZ <marketing@jamz.studio>
2025-06-19 12:50:52 +0000

    feat: atualiza projeto com todas as implementações

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-// src/main.tsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx' // <- importante que seja o .tsx
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // <--- ESSENCIAL
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
